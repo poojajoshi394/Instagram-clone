3f4a21b5b24b1ea705f7ce529802514256041606
priya joshi <priyajoshi@priyas-MacBook-Pro.local>
2024-07-15 16:31:56 +0530

    added cloudenary

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { FlatList, View } from 'react-native';
-import posts from '../../../assets/data/posts.json';
 import PostListItem from '../../components/PostListItems';
-
-
+import posts from '../../../assets/data/posts.json'
 export default function FeedScreen() {
   return (
     <FlatList
       data={posts}
       renderItem={({ item }) => <PostListItem post={item} />}
-      contentContainerStyle={{ gap: 10 ,maxWidth:512,alignSelf: 'center' ,width:'100%'}}
+      contentContainerStyle={{ gap: 10 }}
       showsVerticalScrollIndicator={false}
     />
   );
--- src/components/PostListItems.tsx
+++ src/components/PostListItems.tsx
@@ -1,31 +1,45 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, useWindowDimensions } from 'react-native';
 import { Ionicons, Feather, AntDesign } from '@expo/vector-icons';
-export default function PostListItem(props) {
-  const post=props.post;
-    return(
-        <View className="bg-white">
-        {/* Header */}
-        <View className="p-3 flex-row items-center gap-2 mb-3">
-          <Image
-            source={{ uri: post.user.image_url }}
-            className="w-12 aspect-square rounded-full"
-          />
-          <Text className="font-semibold">{post.user.username}</Text>
-        </View>
-        {/* Post Image */}
-        <Image
-          source={{ uri: post.image_url }}
-          className="w-full aspect-[4/3] mb-3"
-        />
-        {/* Post Actions */}
-        <View className="flex-row gap-3 p-3">
-          <AntDesign name="hearto" size={20} />
-          <Ionicons name="chatbubble-outline" size={20} />
-          <Feather name="send" size={20} />
-          <Feather name="bookmark" size={20} className="ml-auto" />
-        </View>
-      </View>
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from 'cloudinary-react-native';
+// Import required actions and qualifiers.
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { cld } from '../lib/cloudinary';
 
-    );
+export default function PostListItem({post}) {
+  const {width}=useWindowDimensions();
+
+  const image = cld .image(post.image);
+   image.resize(thumbnail().width(500).height(500)) ; 
+   const avatar = cld .image(post.user.avatar_url);
+   avatar.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face()))) ;
+
+
+  return (
+    <View className="bg-white mb-3">
+      {/* Header */}
+      <View className="p-3 flex-row items-center gap-2 ">
+        <AdvancedImage
+        cldImg={avatar}
+          className="w-12 aspect-square rounded-full"
+        />
+        <Text className="font-semibold">{post.user.username}</Text>
+      </View>
+      {/* Post Image */}
+
+     <AdvancedImage cldImg={image} className="w-full aspect-[4/3]" />
+
+      {/* Post Actions */}
+      <View className="flex-row gap-3 p-3">
+        <AntDesign name="hearto" size={20} />
+        <Ionicons name="chatbubble-outline" size={20} />
+        <Feather name="send" size={20} />
+        <Feather name="bookmark" size={20} className="ml-auto" />
+      </View>
+    </View>
+  );
 }
